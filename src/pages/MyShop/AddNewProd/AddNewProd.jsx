054fb736867398c4623acc4fc169f566ba20e385
selma17 <lahoualiselma@gmail.com>
2024-05-02 00:06:27 +0100

    product-creation

--- src/pages/MyShop/AddNewProd/AddNewProd.jsx
+++ src/pages/MyShop/AddNewProd/AddNewProd.jsx
@@ -1,16 +1,19 @@
 import axios from "axios";
 import MyShopSideBar from "../../../components/MyShopSideBar/MyShopSideBar";
 import { useState } from "react";
+import "./AddNewProd.css";
 
 const AddNewProd = () => {
+  // product states / product states / product states / product states
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [quantity, setQuantity] = useState("");
   const [error, setError] = useState("");
+  // image upload states / image upload states / image upload states / image upload states
   const [selectedFile, setSelectedFile] = useState(null);
-  const [uploadStatus, setUploadStatus] = useState("");
+  const [uploadStatus] = useState("");
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -26,13 +29,17 @@ const AddNewProd = () => {
     formData.append("category", category);
     formData.append("quantity", quantity);
     formData.append("image", selectedFile);
-
+    // product creation / product creation / product creation / product creation
     try {
-      const response = await axios.post("http://localhost:3000/products/", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
+      const response = await axios.post(
+        "http://localhost:3000/products/",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
 
       if (response.status === 200) {
         console.log(response.data, "product created successfully");
@@ -44,31 +51,52 @@ const AddNewProd = () => {
       setError("Failed to create product.");
     }
   };
-
   return (
     <>
       <MyShopSideBar />
       <form onSubmit={handleSubmit} className="add-new-product">
+        {/* product name / product name / product name / product name  */}
         <label>Name:</label>
-        <input type="text" placeholder="Enter Your Product Name" onChange={(e) => setName(e.target.value)} />
-
+        <input
+          type="text"
+          placeholder="Enter Your Product Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        {/* product description / product description / product description */}
         <label>Description:</label>
-        <input type="text" placeholder="Enter Your Product Description" onChange={(e) => setDescription(e.target.value)} />
-
+        <input
+          type="text"
+          placeholder="Enter Your Product Description"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        {/* product price / product price / product price / product price */}
         <label>Price:</label>
-        <input type="number" placeholder="Enter The Price" onChange={(e) => setPrice(e.target.value)} />
-
+        <input
+          type="number"
+          placeholder="Enter The Price"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        {/* product category / product category / product category / product category  */}
         <label>Category:</label>
-        <select id="category" name="category" value={category} onChange={(e) => setCategory(e.target.value)}>
+        <select
+          id="category"
+          name="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
           <option value="cloth">clothing</option>
           <option value="accessory">accessory</option>
           <option value="shoes">shoes</option>
           <option value="home decoration">home decoration</option>
         </select>
-
+        {/* product quantity / product quantity / product quantity / product quantity  */}
         <label>Quantity:</label>
-        <input type="number" placeholder="Enter The Quantity" onChange={(e) => setQuantity(e.target.value)} />
-
+        <input
+          type="number"
+          placeholder="Enter The Quantity"
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+        {/* image upload / image upload / image upload / image upload / image upload */}
         <input type="file" accept="image/*" onChange={handleFileChange} />
         <button type="submit">Add Product</button>
       </form>
--- src/pages/MyShop/Shop/MyShop.jsx
+++ src/pages/MyShop/Shop/MyShop.jsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
 import MyShopSideBar from "../../../components/MyShopSideBar/MyShopSideBar";
+import "./MyShop.css";
+import axios from "axios";
+import { BiSolidLike } from "react-icons/bi";
+
 const MyShop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/products/`).then((res) => {
+      setProducts(res.data.products);
+    });
+  }, []);
+
   return (
-    <div>
+    <>
       <MyShopSideBar></MyShopSideBar>
-    </div>
+      <div className="products-display">
+        <ul>
+          {products.map((product, index) => (
+            <div key={index}>
+              <li>
+                <img
+                  src={product.image}
+                  alt="image not found"
+                  className="product-image"
+                />
+                {/* Display image */}
+                <div className="product-info">
+                  <div className="prod-name-price">
+                    <h4>{product.name}</h4>
+                    <h4>{product.price} $</h4>
+                  </div>
+                  <h1>{product.category}</h1>
+                  <h1>{product.quantity}</h1>
+                  <p className="product-reaction">
+                    {product.likes} <BiSolidLike />
+                  </p>
+                  <h4 className="add-to-cart">Modify Product</h4>
+                </div>
+              </li>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
