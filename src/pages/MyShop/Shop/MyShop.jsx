5b9152d61982cf78b4577bb097acf765119877dc
selma17 <lahoualiselma@gmail.com>
2024-05-02 18:39:26 +0100

    myShop-prod-UI

--- src/pages/MyShop/Shop/MyShop.jsx
+++ src/pages/MyShop/Shop/MyShop.jsx
@@ -4,47 +4,62 @@ import "./MyShop.css";
 import axios from "axios";
 import { BiSolidLike } from "react-icons/bi";
 import { RiEdit2Fill } from "react-icons/ri";
+import { Pagination } from "@mui/material";
 
 const MyShop = () => {
   const [products, setProducts] = useState([]);
-
+  // pagination / pagination / pagination / pagination / pagination / pagination
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+  // get all products / get all products / get all products / get all products / get all products
   useEffect(() => {
-    axios.get(`http://localhost:3000/products/`).then((res) => {
+    axios.get(`http://localhost:3000/products?page=${page}`).then((res) => {
       setProducts(res.data.products);
+      setTotalPages(res.data.totalPages);
     });
-  }, []);
+  }, [page]);
 
   return (
     <>
       <MyShopSideBar></MyShopSideBar>
-      <div className="my-products-display">
-        <ul>
+      <div className="my-products-position">
+        <ul className="my-products-display">
           {products.map((product, index) => (
             <div key={index}>
               <li className="my-products">
+                {/* Display image */}
                 <img
                   src={product.image}
                   alt="image not found"
                   className="my-product-image"
                 />
-                {/* Display image */}
                 <div className="my-product-info">
                   <div className="my-prod-name-price">
                     <h4>{product.name}</h4>
-                    <h4>{product.price} $</h4>
+                    <pattern>{product.price} $</pattern>
                   </div>
-                  <h4>{product.category}</h4>
-                  <h4> Quantity {product.quantity}</h4>
+                  <p>Category : {product.category}</p>
+                  <p> Quantity {product.quantity}</p>
                   <p className="my-product-likes">
-                    {product.likes} <BiSolidLike />
+                    <BiSolidLike className="my-product-icon" /> {product.likes}
                   </p>
-                  <h4 className="modify-Product">  <RiEdit2Fill /> Modify Product</h4>
+                  <h4 className="modify-Product">
+                    <RiEdit2Fill className="my-product-icon" /> Modify Product
+                  </h4>
                 </div>
-               
               </li>
             </div>
           ))}
         </ul>
+        <Pagination
+          page={page}
+          count={totalPages}
+          onChange={handlePageChange}
+          className="pagination"
+        />
       </div>
     </>
   );
