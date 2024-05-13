7e2c9ceb894b45db2cc2cc0ca2229bbe1a59850d
selma17 <lahoualiselma@gmail.com>
2024-05-14 00:00:12 +0100

    FIX STORE 2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../redux/slices/AuthSlice";
 import CreatShopSlice from "./slices/CreatShopSlice";
-export const store = configureStore({
+export const Store = configureStore({
   reducer: {
     auth: authSlice,
     shop: CreatShopSlice,
